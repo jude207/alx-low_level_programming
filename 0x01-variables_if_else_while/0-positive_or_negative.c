9ca9715f6295ea6b24526a42df71e4a7ec51a41d
jude207 <judeuche207gmail.com>
2022-06-03 22:32:25 -0700

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each
- * Return : always 0
+ * main - assign a random number each time it is executed
+ * Return: always 0: 
  */
 int main(void)
 {
