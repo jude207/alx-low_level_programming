4de544da6a87a237bc6501a77e949b67b5d3c4a4
jude207 <judeuche207gmail.com>
2022-06-03 19:53:10 -0700

    fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* A programm to print out positive or negative number*/
 int main(void)
 {
 	int n;
