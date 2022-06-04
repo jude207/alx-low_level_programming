58dc3f1e3e49b484eb180a29d7466e154f9b63ab
jude207 <judeuche207gmail.com>
2022-06-03 20:01:40 -0700

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* A programm to print out positive or negative number*/
+/**:
+ *main -> assign a random number to the variable n each
+ *time it is executed and printed based on the condition specified
+ *Return : always 0 
+ */
 int main(void)
 {
 	int n;
