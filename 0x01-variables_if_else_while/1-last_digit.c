b7e2762a3ff4fcdedf50d89e18c1e118084a4a27
jude207 <judeuche207gmail.com>
2022-06-06 05:25:32 -0700

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  * main - assign a random number to the variable n each time it is executed
@@ -11,22 +11,22 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is ", n);
-	if (n % 10 > 5)
-	{	
-		printf("%d and is greater than 5 ", n % 10);
-	}	
-	if (n % 10 == 0)
-	}
-		printf("%d and is 0", n % 10);
-	}
-	if ((n % 10 < 6) && (n % 10 != 0))
+	last = n % 10;
+	if (last > 5)
 	{
-		printf("%d and is less than 6 and not 0", n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	else if (last == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
-	
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,6 +12,10 @@ int main(void)
 
 	while (ch <= 'z')
 	{
+		if ((ch == 'e') || (ch == 'q'))
+		{
+			continue;
+		}
 		putchar(ch);
 		ch++;
 	}
