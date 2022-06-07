b29a923d2375d0ef923e3747259e62de8cf8916b
jude207 <judeuche207gmail.com>
2022-06-06 19:45:03 -0700

    single digit number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-prints single number f base 10
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
