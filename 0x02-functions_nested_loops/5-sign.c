ede5b826d0dfcdbd0305f439df8464dbbf61bfc6
jude207 <judeuche207gmail.com>
2022-06-07 20:54:29 -0700

    chech for sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print a positive or negative sign
+ * @n: take in an int
+ * Return: returns 0,1,-1.
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
