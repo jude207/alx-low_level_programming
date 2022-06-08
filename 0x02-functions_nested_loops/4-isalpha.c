b75df8aedcef9557aa46a4174f6332110a0fed5e
jude207 <judeuche207gmail.com>
2022-06-07 20:08:15 -0700

    _isalpa

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character entered is an alphab.
+ * @c: our function takes charcter input c.
+ * Return: returns true or false
+ */
+
+int _isalpha(int c)
+{
+	return ((c => A || c <= Z) && (c => 'a' || c <= 'z'));
+}
