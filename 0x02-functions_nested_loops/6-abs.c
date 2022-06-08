81be7b56f06229903516c07867badaff6fad863b
jude207 <judeuche207gmail.com>
2022-06-07 21:27:44 -0700

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs- give the abs value of any number
+ * @n:is the int parameter to be converted
+ * Return: returns n_abs value.
+ */
+
+int _abs(int n)
+{
+	int n_abs;
+
+	n_abs = abs(n);
+	return (n_abs);
+}
