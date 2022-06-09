96bd91ba1c37ab375926426aa7e4ea198c80898e
jude207 <judeuche207gmail.com>
2022-06-09 06:20:27 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0-9
+ * Return: no return value
+ */
+
+void print_numbers(void)
+{
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
