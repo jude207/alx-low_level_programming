329d20caa64c14433fdfaf3ea1cf7fbb74d88978
jude207 <judeuche207gmail.com>
2022-06-09 06:01:27 -0700

    fixed

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *mul- multiplies two number
- *@a- input first parameter
- *@b- input second parameter
- *Return: returns result
+ * mul- multiplies two number
+ * @a: input first parameter
+ * @b: input second parameter
+ * Return: returns result
  */
 
 int mul(int a, int b)
