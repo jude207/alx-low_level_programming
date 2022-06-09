935cbc656189b5e1cde9618a834fb518a25a29fd
jude207 <judeuche207gmail.com>
2022-06-09 12:56:06 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_number - prints number excluding 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+	while (i <= 9)
+	{
+		while (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}	
+		_putchar(i);
+		i++;
+	}_putchar('\n');
+}
