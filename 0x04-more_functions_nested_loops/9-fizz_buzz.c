121b59a17149230db75ac6bc9d26c91698d98435
jude207 <judeuche207gmail.com>
2022-06-12 16:08:15 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * program that print fizz and buzz in
+ * place of 3 and 5
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fuzz ");
+		}
+		else
+		{
+			printf("%d ", i)
+		}
+	}
+}
