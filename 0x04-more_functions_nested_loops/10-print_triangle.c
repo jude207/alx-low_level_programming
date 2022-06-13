aa2cf0455098bbe4e3c9300ef3a0dc11f2f03a84
jude207 <judeuche207gmail.com>
2022-06-12 18:46:56 -0700

    fixed

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i,j;
+
+	if(size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+ 				_putchar('#');
+
+			}
+
+			_putchar('\n');
+		}
+		
+	}
+	
+}	
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz\t");
+			printf("FizzBuzz''");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\t");
+			printf("Buzz''");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fuzz\t");
+			printf("Fuzz''");
 		}
 		else
 		{
-			printf("%d\t", i);
+			printf("%d''", i);
 		}
 	}
 	return (0);
