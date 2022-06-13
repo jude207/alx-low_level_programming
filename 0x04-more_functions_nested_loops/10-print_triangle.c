cee82fb824ff3edd4f510784e3ae086bf66df79e
jude207 <judeuche207gmail.com>
2022-06-12 19:01:57 -0700

    correted

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_triangle -print a right handed triangle
+ * @size:takes input size
+ * Return: returns nothing
  */
 
 void print_triangle(int size)
 {
-	int i,j;
+	int i, j;
 
-	if(size > 0)
+	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
@@ -17,16 +18,14 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-
 			for (j = 1; j <= i; j++)
 			{
- 				_putchar('#');
+				_putchar('#');
 
 			}
 
+
 			_putchar('\n');
 		}
-		
 	}
-	
-}	
+}
