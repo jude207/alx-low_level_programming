fc577b59cbe2528c6109944aa47a63a30b75556e
jude207 <judeuche207gmail.com>
2022-06-13 20:40:33 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -> swaps two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}	
