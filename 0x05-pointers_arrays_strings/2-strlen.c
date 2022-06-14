c1c8c1eaa98a67db03ec26959562755566cab972
jude207 <judeuche207gmail.com>
2022-06-13 21:07:38 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to get lenght of any sting
+ * @s: string pointer as parameter to fun
+ * Return: returns lenght
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
