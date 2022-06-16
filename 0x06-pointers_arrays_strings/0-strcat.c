9d622e4df824950de8de7f77ee3e02e2a346b944
jude207 <judeuche207gmail.com>
2022-06-16 04:23:47 -0700

    string cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - this function join two words
+ * @dest: first param
+ * @src: second param
+ * Return: returns string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
