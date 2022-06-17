7838e0204ef481d39602aa5f2eb9cb69122d15de
jude207 <judeuche207gmail.com>
2022-06-16 18:33:02 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int st = 0;
+
+	*(a + 0) = *(a - j);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%p", ptr);
+		ptr++;
+	}
+}	
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts any sting to upper
+ * @x: parameter
+ * Return: a string in capital form
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+		{
+			x[i] = x[i] - 32;
+		}
+		i++;
+	}
+	return (x);
+}
