965de0317f564a54b560e44c3932bdd32c79b38b
jude207 <judeuche207gmail.com>
2022-06-20 10:29:10 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/**unsigned int i;*/
+
+	return (memset(s, b, n));
+
+	/**for (i = 0; i < n; i++)
+	{
+		printf("%c", *(s + n));
+	}
+	printf("\n");
+	return (0);*/
+}
