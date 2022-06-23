22fc3794659d582074a56fb2a8a942eb039a232d
jude207 <judeuche207gmail.com>
2022-06-22 21:58:56 -0700

    fix

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int is _prime_number(int n)
+{
+	return (check_prime(n, 1));
+}	
+
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	if ((n / i) < i)
+	{
+		return (1);
+	}
+	return (check_prime(n, i + 1));
+}
