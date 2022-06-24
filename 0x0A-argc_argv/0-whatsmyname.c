de9def072d635c258c203d8de7182ad00f4a0055
jude207 <judeuche207gmail.com>
2022-06-24 08:45:56 -0700

    prints name of file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the name of the file
+ * @argc: is the number element our argv can hold
+ * @argv: an array that hold element we pass in
+ * Return: returns nothing
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -8,6 +15,6 @@ int main(int argc, char *argv[])
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
-	}	
+	}
 	return (0);
-}	
+}
