97e9cf2035f70497a2dd49b42f503c2316e43853
jude207 <judeuche207gmail.com>
2022-06-24 13:31:50 -0700

    print number of string in argv array

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * main - print the number of string argv has
+ * @argc: the number of string element in argv
+ * @argv: array of string
+ * Return: returns 0
  */
-int main(int argc, char *argv)
+
+int main(int argc, char *argv[])
 {
-	printf("%d", argc);
+	if (argv != NULL)
+	{
+		printf("%d\n", argc);
+	}
 	return (0);
-}	
+}
