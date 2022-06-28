209012bb72752655b4b43caf2395ff0ef376aa66
jude207 <judeuche207gmail.com>
2022-06-28 04:52:59 -0700

    allocates memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - allocate memory to the char
+ * @size: length of the char
+ * @c: char to be allocated memory
+ * Returns: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	p = (char *)malloc(sizeof(char) * (size));
+	if (p == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
