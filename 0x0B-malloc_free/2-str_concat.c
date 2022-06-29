866077e767bc430e324bb1b17e30926680250ed3
jude207 <judeuche207gmail.com>
2022-06-28 21:47:00 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,40 +16,26 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = " ";
-	}
+		s2 = "";
 	while (s1[i])
-	{
 		i++;
-	}
 	while (s2[j])
-	{
 		j++;
-	}
 
 	l = i + j;
 	s = (char *)malloc(l * sizeof(char) + 1);
 	if (s == NULL)
-	{
-		return (0);
-	}
+		return (NULL);
 	j = 0;
 	while (k < l)
 	{
 		if (k < i)
-		{
 			s[k] = s1[k];
-		}
 		if (k >= i)
-		{
 			s[k] = s2[j];
 			j++;
-		}
 		k++;
 	}
 	s[k] = '\0';
