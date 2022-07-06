85f877f22e1f2739c61243e4d1a88e309f462cbd
jude207 <judeuche207gmail.com>
2022-07-06 15:18:59 -0700

    corrected

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,8 +9,18 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
+	if (array == NULL)
+	{
+		return;
+	}
+	if (action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
