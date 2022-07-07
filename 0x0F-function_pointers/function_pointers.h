d7fca1e0683a83451c40eb8146e04e5af1ea3508
jude207 <judeuche207gmail.com>
2022-07-07 09:50:08 -0700

    search int

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,9 @@
+#include <stddef.h>
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION POINTER_H
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
